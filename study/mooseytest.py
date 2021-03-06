7f93eebfef8365057508bf7c5fd738baeb11f77f
Moose <xacriimony@gmail.com>
2021-03-06 02:08:39 -0600

    Add files via upload

--- study/mooseytest.py
+++ study/mooseytest.py
@@ -19,17 +19,27 @@ class Mooseytest(commands.Cog):
         author = ctx.author
         store_roles = self.config.user(author).roles
         
-        if 817614968127881236 in author.roles:
+        if 817614968127881236 in author.roles or 817568189461299200 in author.roles:
             for r in store_roles:
                 await author.add_roles(r)
                 await store_roles.remove(r)
             await author.remove_roles(817614968127881236)
+            await author.remove_roles(817568189461299200)
             ctx.send('{0} has finished studying!'.format(ctx.author))
         else:
             for r in author.roles:
                 await store_roles.append(r)
                 await author.remove_roles(r)
             await author.add_roles(817614968127881236)
+            await author.add_roles(817568189461299200)
             ctx.send('{0} has been sent to study purgatory!'.format(ctx.author))
         
         self.config.user(author).roles = store_roles
+        
+    @commands.command()
+    async def printallroles(self, ctx):
+        """moosey test!"""
+        out = ""
+        for r in author.roles:
+            out += str(r) + "\n"
+        ctx.send('{}'.format(out))
