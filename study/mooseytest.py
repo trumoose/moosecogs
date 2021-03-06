0aaab0e50fb8e81dc72bbb5f097295a27681e5b2
Moose <xacriimony@gmail.com>
2021-03-06 09:54:08 -0600

    Add files via upload

--- study/mooseytest.py
+++ study/mooseytest.py
@@ -22,7 +22,7 @@ class Mooseytest(commands.Cog):
 
         async with self.config.user(ctx.author).roles() as roles:
             if studying in ctx.author.roles:
-                    await ctx.author.add_roles(*roles)
+                    await ctx.author.add_roles(roles)
                     roles.clear()
                 await ctx.author.remove_roles(studying)
                 await ctx.tick()
@@ -33,7 +33,7 @@ class Mooseytest(commands.Cog):
                         #roles.append(r.id)
                     #await ctx.author.remove_roles(*roles)
                 #await ctx.author.add_roles(studying)
-                await ctx.tick()
+                #await ctx.tick()
                 await ctx.send('{0} has been sent to study purgatory!'.format(ctx.author.name))
         
     @commands.command()
