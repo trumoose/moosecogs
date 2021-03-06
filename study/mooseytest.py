a444b9e533dcdfc87fdff207b1b01a6e5e68c905
Moose <xacriimony@gmail.com>
2021-03-06 09:51:39 -0600

    Add files via upload

--- study/mooseytest.py
+++ study/mooseytest.py
@@ -20,21 +20,21 @@ class Mooseytest(commands.Cog):
         
         studying = discord.utils.get(ctx.guild.roles, name='study')
 
-        if studying in ctx.author.roles:
-            async with self.config.user(ctx.author).roles() as roles:
-                await ctx.author.add_roles(*roles)
-                roles.clear()
+        async with self.config.user(ctx.author).roles() as roles:
+            if studying in ctx.author.roles:
+                    await ctx.author.add_roles(roles)
+                    roles.clear()
+                await ctx.author.remove_roles(studying)
                 await ctx.tick()
-            await ctx.author.remove_roles(studying)
-            await ctx.send('{0} has finished studying!'.format(ctx.author.name))
-        else:
-            #async with self.config.user(ctx.author).roles() as roles:
-                #for r in ctx.author.roles:
-                    #roles.append(r.id)
-                #await ctx.author.remove_roles(*roles)
-                #await ctx.tick()
-            #await ctx.author.add_roles(studying)
-            await ctx.send('{0} has been sent to study purgatory!'.format(ctx.author.name))
+                await ctx.send('{0} has finished studying!'.format(ctx.author.name))
+            else:
+                #async with self.config.user(ctx.author).roles() as roles:
+                    #for r in ctx.author.roles:
+                        #roles.append(r.id)
+                    #await ctx.author.remove_roles(*roles)
+                #await ctx.author.add_roles(studying)
+                await ctx.tick()
+                await ctx.send('{0} has been sent to study purgatory!'.format(ctx.author.name))
         
     @commands.command()
     async def printallroles(self, ctx):
