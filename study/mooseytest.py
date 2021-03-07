d55d21662bf47c6615debf80ebe0d9df43f5b0d5
Moose <xacriimony@gmail.com>
2021-03-07 00:03:22 -0600

    Add files via upload

--- study/mooseytest.py
+++ study/mooseytest.py
@@ -64,7 +64,7 @@ class Mooseytest(commands.Cog):
         unit_spent_studying = await self.config.member(ctx.author).unitStudying()
         
         if await self.config.member(ctx.author).recursion() and await self.config.member(ctx.author).timerInProgress():
-            await ctx.send("**{}** has finished studying after {} {}s!".format(ctx.author.name, time_spent_studying, unit_spent_studying))
+            #await ctx.send("**{}** has finished studying after {} {}s!".format(ctx.author.name, time_spent_studying, unit_spent_studying))
             await self.config.member(ctx.author).timerInProgress.set(False)
             await self.config.member(ctx.author).recursion.set(False)
         
