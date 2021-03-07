7a7a526b213501b27fddeda469c76359de5ccab0
Moose <xacriimony@gmail.com>
2021-03-06 22:37:15 -0600

    Add files via upload

--- study/mooseytest.py
+++ study/mooseytest.py
@@ -69,7 +69,7 @@ class Mooseytest(commands.Cog):
             await self.config.member(ctx.author).timerInProgress.set(False)
             
         elif not await self.config.member(ctx.author).studyInProgess():
-            if duration is None or unit_of_time is None:
+            if duration != None and unit_of_time != None:
                 unit_of_time = unit_of_time.lower()
                 
                 s = ""
