497346e22509bb13839dda67cd01261d32fe55b6
Moose <xacriimony@gmail.com>
2021-03-06 22:48:28 -0600

    Add files via upload

--- study/mooseytest.py
+++ study/mooseytest.py
@@ -30,7 +30,7 @@ class Mooseytest(commands.Cog):
 
     @commands.command()
     @commands.cooldown(1, 5, commands.BucketType.user)
-    async def study(self, ctx, duration = None, unit_of_time = None):
+    async def study(self, ctx, duration = None, unit_of_time = ""):
         """Temporary time-out for those who lack self control."""
         
         studying = discord.utils.get(ctx.guild.roles, name='study')
@@ -69,7 +69,7 @@ class Mooseytest(commands.Cog):
             await self.config.member(ctx.author).timerInProgress.set(False)
             
         elif not await self.config.member(ctx.author).studyInProgess():
-            if duration != None or unit_of_time != None:
+            if duration != None or unit_of_time != "":
                 unit_of_time = unit_of_time.lower()
                 
                 s = ""
