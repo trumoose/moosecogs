2a1b0660c38225234447d33cc93bfb2f327bf185
Moose <xacriimony@gmail.com>
2021-03-09 11:32:55 -0600

    Add files via upload

--- countchart/countchart.py
+++ countchart/countchart.py
@@ -47,7 +47,7 @@ class Countchart(commands.Cog):
             bbox_transform=plt.gcf().transFigure,
             facecolor="#ffffff",
         )
-        plt.subplots(figsize=(10,10))
+        plt.subplots(figsize=(5,5))
         plt.subplots_adjust(left=0.0, bottom=0.1, right=0.45)
         image_object = BytesIO()
         plt.savefig(image_object, format="PNG", facecolor="#36393E")
