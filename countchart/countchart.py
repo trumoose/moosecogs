60e86964accb4f0a950a98b7da0907e2e1df5333
Moose <xacriimony@gmail.com>
2021-03-09 05:06:01 -0600

    Add files via upload

--- countchart/countchart.py
+++ countchart/countchart.py
@@ -21,6 +21,62 @@ class Countchart(commands.Cog):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=1312131213121312, force_registration=True)
         self.config.register_guild(guild_messages = [])
+
+	@staticmethod
+    async def create_chart(top, others):
+        channel = ctx.guild.get_channel(771829982158389258)
+        plt.clf()
+        sizes = [x[1] for x in top]
+        labels = ["{} {:g}%".format(x[0], x[1]) for x in top]
+        if len(top) >= 20:
+            sizes = sizes + [others]
+            labels = labels + ["Others {:g}%".format(others)]
+        if len(channel.name) >= 19:
+            channel_name = "{}...".format(channel.name[:19])
+        else:
+            channel_name = channel.name
+        title = plt.title("Stats in #{}".format(channel_name), color="white")
+        title.set_va("top")
+        title.set_ha("center")
+        plt.gca().axis("equal")
+        colors = [
+            "r",
+            "darkorange",
+            "gold",
+            "y",
+            "olivedrab",
+            "green",
+            "darkcyan",
+            "mediumblue",
+            "darkblue",
+            "blueviolet",
+            "indigo",
+            "orchid",
+            "mediumvioletred",
+            "crimson",
+            "chocolate",
+            "yellow",
+            "limegreen",
+            "forestgreen",
+            "dodgerblue",
+            "slateblue",
+            "gray",
+        ]
+        pie = plt.pie(sizes, colors=colors, startangle=0)
+        plt.legend(
+            pie[0],
+            labels,
+            bbox_to_anchor=(0.7, 0.5),
+            loc="center",
+            fontsize=10,
+            bbox_transform=plt.gcf().transFigure,
+            facecolor="#ffffff",
+        )
+        plt.subplots_adjust(left=0.0, bottom=0.1, right=0.45)
+        image_object = BytesIO()
+        plt.savefig(image_object, format="PNG", facecolor="#36393E")
+        image_object.seek(0)
+        return image_object
 		
         
         
@@ -29,15 +85,25 @@ class Countchart(commands.Cog):
         """Generates a pie chart, representing all the messages in the countchart channel."""
         
         channel = ctx.guild.get_channel(771829982158389258)
-        #message_history = await self.config.guild(ctx.guild).guild_messages()
+        message_history = await self.config.guild(ctx.guild).guild_messages()
 
-        #async for msg in channel.history():
-            #if not msg in message_history:
-                #message_history.append(msg)
-                #await asyncio.sleep(0.005)
+        async for msg in channel.history():
+            if not msg in message_history:
+                message_history.append(msg)
+                await asyncio.sleep(0.005)
 
-        #await self.config.guild(ctx.guild).guild_messages.set(message_history)
+        await self.config.guild(ctx.guild).guild_messages.set(message_history)
         
-        #async for msg in message_history:
-            #await send ctx.send("{}".format(msg))
+        async for msg in message_history:
+            await ctx.send("{}".format(msg))
     
+    @commands.command()
+    async def sendcountchart(self, ctx):
+        message_history = await self.config.guild(ctx.guild).guild_messages()
+        async for msg in message_history:
+            await ctx.send("{}".format(msg))
+        
+    @commands.command()
+    async def emptycountchart(self, ctx):
+        empty = []
+        await self.config.guild(ctx.guild).guild_messages.set(empty)
