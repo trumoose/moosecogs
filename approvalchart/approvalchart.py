5e1085660419061617661a5a86f3243156504555
Moose <xacriimony@gmail.com>
2021-04-05 04:10:10 -0500

    Add files via upload

--- approvalchart/approvalchart.py
+++ approvalchart/approvalchart.py
@@ -36,7 +36,7 @@ class Approvalchart(commands.Cog):
         title.set_va("top")
         title.set_ha("center")
         plt.gca().axis("equal")
-        cmap = plt.cm.rainbow
+        cmap = plt.cm.turbo
         reversed_cmap = cmap.reversed()
         colors = reversed_cmap(np.linspace(0., 1., 21))
         pie = plt.pie(sizes, colors=colors, startangle=0)
