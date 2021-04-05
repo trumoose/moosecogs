5a1313267d4e216fd7cd714c7484d724073c4efe
Moose <xacriimony@gmail.com>
2021-04-05 17:58:02 -0500

    Add files via upload

--- marriage/marriage.py
+++ marriage/marriage.py
@@ -47,15 +47,21 @@ class Marriage(commands.Cog):
         """Set member variables for a user."""
         if var == "married":
             await self.config.member(member).married.set(state)
+            if not state:
+                await self.config.member(member).current.clear()
             await ctx.send(f"Set {member.mention}'s marriage status to {'married!' if state else 'unmarried!'}")
         if var == "divorced":
             await self.config.member(member).divorced.set(state)
             await ctx.send(f"Set {member.mention}'s divorce status to {'divorced!' if state else 'undivorced!'}")
         if var == "parent":
             await self.config.member(member).parent.set(state)
+            if not state:
+                await self.config.member(member).children.clear()
             await ctx.send(f"Set {member.mention}'s parental status to {'parent!' if state else 'not a parent!'}")
         if var == "child":
             await self.config.member(member).child.set(state)
+            if not state:
+                await self.config.member(member).parents.clear()
             await ctx.send(f"Set {member.mention}'s child status to {'child!' if state else 'orphan!'}")
         if var == "marcount":
             await self.config.member(member).marcount.set(state)
